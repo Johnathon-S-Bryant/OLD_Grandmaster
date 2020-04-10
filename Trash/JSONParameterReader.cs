f8e8a75cb0104127b4a30e0f489f5ff4d1904875
Johnathon-S-Bryant <johnathon.s.bryant@gmail.com>
2020-04-10 16:13:54 -0500

    Hopefully should not be 0 bytes now

---
+++ Trash/JSONParameterReader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class JSONParameterReader
+{
+    public static int[] ReadParams(string filepath)
+    {
+        int[] ret = new int[(int)StatTypes.Count];
+        //JsonUtility.FromJson
+        //System.IO.File.ReadAllText(filepath);
+        return ret;
+    }
+}
